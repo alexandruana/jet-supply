10ba1dcc52f7f484c45a07c712cb922d61e1b26d
alexandruana <anaalexandru1996@gmail.com>
2021-09-03 10:09:24 +0200

    Added arrivalMarker & departureMarker function

--- resources/js/Components/TripMap.vue
+++ resources/js/Components/TripMap.vue
@@ -1,77 +1,50 @@
 <template>
     <div id="map"></div>
+    <div id="distance" class="distance-container"></div>
 </template>
 
 <script>
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
+import length from "@turf/length";
 
 export default {
     name: "TripMap",
+    data() {
+        return {
+            departureMarker: null,
+            arrivalMarker: null,
+            route: null,
+            map: null
+        }
+    },
     mounted() {
         this.initMapBox()
     },
     watch: {
-      departure: function(val) {
-          // Example of a MapDataEvent of type "sourcedata"
-          console.log("Departure added.")
-      },
-      arrival: function(val) {
-          console.log("Arrival added.")
-      }
+        departure: function(val) {
+            this.departureMarker.setLngLat([val.longitude, val.latitude])
+                .addTo(this.map)
+        },
+        arrival: function(val) {
+          this.arrivalMarker.setLngLat([val.longitude, val.latitude])
+              .addTo(this.map)
+        }
     },
     methods: {
+        // Initialize MapBox map
         initMapBox: function() {
             mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGFuZHJ1YW5hIiwiYSI6ImNrZTl3NzJ3bzIxNG4yc2w2aG03dHNkMDUifQ.xaSxrVMLZtfGAlWoGvB1PQ';
-            const map = new mapboxgl.Map({
+             this.map = new mapboxgl.Map({
                 container: 'map',
                 style: 'mapbox://styles/alexandruana/cksxeq0637zjy17tcvd4h919d',
                 center: [22.253, 45.419],
                 zoom: 6
             });
 
-            const airports = {
-                "type": "FeatureCollection",
-                "features": [
-                    {
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [
-                                21.337,
-                                45.809
-                            ]
-                        }
-                    },
-                    {
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [
-                                22.254,
-                                45.41998
-                            ]
-                        }
-                    },
-                ]
-            }
-
-            map.on('load', function() {
-                /* Add the data to your map as a layer */
-                map.addSource('places', {
-                    'type': 'geojson',
-                    'data': airports
-                });
-                function addMarkers() {
-                    for( const marker of airports.features ) {
-                        new mapboxgl.Marker()
-                            .setLngLat(marker.geometry.coordinates)
-                            .addTo(map);
-                    }
-                }
-                addMarkers()
-            });
+             // Set markers on input
+            this.arrivalMarker = new mapboxgl.Marker({})
+            this.departureMarker = new mapboxgl.Marker({})
         },
-
     },
     props: {
         departure: Object,
