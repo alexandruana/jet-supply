a0fc6f6bd16eefca2b435129c284787a2ec44226
alexandruana <anaalexandru1996@gmail.com>
2021-09-14 22:44:25 +0200

    Initial commit.

---
+++ src/components/PairingSearch.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="grid grid-cols-2 gap-5 rounded-lg border-2 shadow-lg max-w-lg mx-auto py-8 px-5 my-10 bg-gray-50">
+    <autocomplete-input label="From" placeholder="From where?" :items="allAirports"></autocomplete-input>
+  </div>
+</template>
+
+<script>
+import AutocompleteInput from "@/components/partials/AutocompleteInput";
+import {mapGetters} from "vuex";
+export default {
+  name: "PairingSearch",
+  computed: {
+    ...mapGetters(['allAirports'])
+  },
+  components: {
+    AutocompleteInput
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/partials/AutocompleteInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <label class="block">
+    <span class="text-gray-700 font-bold">{{ label }}</span>
+    <input
+        type="search"
+        class="mt-1 block rounded-md border-0 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 bg-transparent"
+        :placeholder="placeholder"
+        v-model="keyword"
+        @input="search"
+    >
+    <ul v-show="hasInput">
+      <li>
+        hi
+      </li>
+    </ul>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "AutocompleteInput",
+  data() {
+    return {
+      keyword: '',
+      results: [],
+      hasInput: false,
+    }
+  },
+  methods: {
+    filterResults: function() {
+      this.results = this.items.filter((item) => {
+        item.toLowerCase() === '';
+      })
+    },
+    search: function() {
+      this.hasInput = true;
+      this.filterResults();
+    }
+  },
+  props: {
+      label: String,
+      placeholder: String,
+      items: {
+        type: Array,
+        required: false
+      },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
