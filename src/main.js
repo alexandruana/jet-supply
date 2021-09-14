766623ade7f8dcc8eaa6baf03fa09e813998c840
alexandruana <anaalexandru1996@gmail.com>
2021-09-14 23:13:39 +0200

    Revert "Initial commit."
    This reverts commit a0fc6f6b

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
-import axios from 'axios'
-
-import '@/styles/tailwind.css'
-import 'tailwindcss/tailwind.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  axios,
   render: h => h(App),
 }).$mount('#app')
