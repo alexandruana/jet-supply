f97a6f723bf8a9b5a866e335d587e7dd078c6d79
alexandru ana <anaalexandru1996@gmail.com>
2021-10-05 20:26:17 +0200

    Added demo navbar.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,12 @@
 <template>
-    <nav class="flex items-center justify-between flex-wrap p-6 bg-white shadow-lg">
-        <span class="font-semibold text-gray-500 text-lg">JetSupply</span>  
-    </nav>
+    <header class="flex items-center justify-between flex-wrap bg-white shadow-lg">
+        <div class="container mx-auto flex flex-wrap p-6 flex-col md:flex-row items-center">
+            <a class="flex title-font font-medium items-center text-gray-700 mb-4 md:mb-0">
+                <p class="ml-3 text-xl font-black">COMPANY<span class="text-blue-500">NAME</span></p>
+            </a>
+             <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                <NuxtLink to="/" class="mr-5 hover:text-gray-700 font-semibold">About</NuxtLink>
+            </nav>
+        </div>
+    </header>
 </template>
