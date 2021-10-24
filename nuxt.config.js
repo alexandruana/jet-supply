560327fc79ef69c894920c4d980a1064e813448c
alexandru ana <anaalexandru1996@gmail.com>
2021-10-24 17:30:50 +0200

    Attempt to initialise MapBox as plugin.

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/datepicker.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -44,6 +45,14 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    loaders: {
+      sass: {
+        implementation: require('sass'),
+      },
+      scss: {
+        implementation: require('sass'),
+      },
+    }
   },
   target: 'static'
 }
