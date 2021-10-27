5fbe3339d96502fe3fa13f7f5bbb520a37801900
alexandruana <36643632+alexandruana@users.noreply.github.com>
2021-10-27 15:05:30 +0200

    Centered NuxtLinks in nav.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,8 +4,8 @@
             <a class="flex title-font font-medium items-center text-jet-blue mb-4 md:mb-0">
                 <p class="ml-3 text-xl font-black">JET APP</span></p>
             </a>
-             <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                <NuxtLink to="/" class="mr-5 text-jet-blue hover:text-jet-blue-light font-semibold">About</NuxtLink>
+             <nav class="md:mx-auto flex flex-wrap items-center text-base justify-center">
+                <NuxtLink to="/" class="text-jet-blue hover:text-jet-blue-light font-semibold">About</NuxtLink>
             </nav>
         </div>
     </header>
--- components/PairingSearch.vue
+++ components/PairingSearch.vue
@@ -1,22 +1,27 @@
 <template>
-     <div class="grid grid-cols-3 gap-5 rounded-lg border-2 max-w-2xl mx-auto py-8 px-5 my-10 bg-gray-50">
-        <SearchInput icon="aircraft-depart" placeholder="Departure" type="departure" />
-        <SearchInput icon="aircraft-land" placeholder="Arrival" type="arrival" />
-        <DateInput />
-    </div>
+  <div
+    class="grid sm:grid-cols-1 md:grid-cols-3 md:gap-5 rounded-lg border-2 max-w-2xl mx-auto py-8 px-5 my-10 bg-gray-50"
+  >
+    <SearchInput
+      icon="aircraft-depart"
+      placeholder="Departure"
+      type="departure"
+    />
+    <SearchInput icon="aircraft-land" placeholder="Arrival" type="arrival" />
+    <DateInput />
+  </div>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
-import { mapGetters } from 'vuex'
+import { defineComponent } from "@vue/composition-api";
+import { mapGetters } from "vuex";
 
 export default defineComponent({
-    setup() {    
-    },
-    computed: {
-        ...mapGetters({
-            getAirports: 'getAirports'
-        })
-    },
-})
+  setup() {},
+  computed: {
+    ...mapGetters({
+      getAirports: "getAirports",
+    }),
+  },
+});
 </script>
