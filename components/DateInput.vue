eee2a668053d2408f278fd8fffa62511d317225f
alexandruana <36643632+alexandruana@users.noreply.github.com>
2021-10-27 15:16:36 +0200

    Centered search inputs on mobile.

--- components/DateInput.vue
+++ components/DateInput.vue
@@ -1,39 +1,34 @@
 <template>
-      <div class="border-b-2 flex flex-col border-jet-blue-light">
-          <client-only>
-            <datepicker 
-                v-model="date"
-                color="#00A9E2"
-            >
-            </datepicker>
-          </client-only>
-      </div>
+  <div class="border-b-2 flex flex-col border-jet-blue-light">
+    <client-only>
+      <datepicker v-model="date" color="#00A9E2"> </datepicker>
+    </client-only>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
-
-
+import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
-    data() {
-        return {
-            date: null
-        }
-    }
-})
+  data() {
+    return {
+      date: null,
+    };
+  },
+});
 </script>
 
 <style lang="scss">
-    .vd-wrapper {
-        .vd-picker__input {
-            &-icon {
-                margin: 0;
-            }
-            input {
-                padding: 0;
-                margin: 0;
-            }
-        }
+.vd-wrapper {
+  padding: 0.5rem;
+  .vd-picker__input {
+    &-icon {
+      margin: 0;
     }
+    input {
+      padding: 0;
+      margin: 0;
+    }
+  }
+}
 </style>
