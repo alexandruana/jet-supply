fe59c4621311326fb5fa31506e6acae3e7949fa8
alexandru ana <anaalexandru1996@gmail.com>
2021-10-29 17:22:41 +0200

    Set arc on('sourcedataloading')

--- components/MapBox.vue
+++ components/MapBox.vue
@@ -7,7 +7,7 @@
 <script>
 import { defineComponent } from "@vue/composition-api";
 import { point } from "@turf/helpers";
-import { greatCircle } from "@turf/turf";
+import greatCircle from "@turf/great-circle";
 import { mapGetters } from "vuex";
 import mapboxgl from "mapbox-gl";
 
@@ -18,13 +18,6 @@ export default defineComponent({
         type: "FeatureCollection",
         features: [],
       },
-      linestring: {
-        'type': 'Feature',
-        'geometry': {
-            'type': 'LineString',
-            'coordinates': []
-        }
-    },
       map: null,
     };
   },
@@ -35,7 +28,6 @@ export default defineComponent({
     departure: function (val, oldVal) {
       if (val) {
         this.addPoint(val);
-        this.getRoute();
       } else if (oldVal) {
         this.removePoint(oldVal);
       }
@@ -43,7 +35,6 @@ export default defineComponent({
     arrival: function (val, oldVal) {
       if (val) {
         this.addPoint(val);
-        this.getRoute();
       } else if (oldVal) {
         this.removePoint(oldVal);
       }
@@ -57,43 +48,68 @@ export default defineComponent({
     initMapBox: function () {
       mapboxgl.accessToken =
         "pk.eyJ1IjoiYWxleGFuZHJ1YW5hIiwiYSI6ImNrZTl3NzJ3bzIxNG4yc2w2aG03dHNkMDUifQ.xaSxrVMLZtfGAlWoGvB1PQ";
-      this.map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/alexandruana/cksxeq0637zjy17tcvd4h919d",
-        center: [22.253, 45.419],
-        zoom: 4,
-      });
+        this.map = new mapboxgl.Map({
+            container: "map",
+            style: "mapbox://styles/alexandruana/cksxeq0637zjy17tcvd4h919d",
+            center: [22.253, 45.419],
+            zoom: 4,
+        });
 
-      this.map.on("load", () => {
-        this.map.addSource("geojson", {
-          type: "geojson",
-          data: this.geojson,
-        });
-        this.map.addLayer({
-          id: "measure-points",
-          type: "circle",
-          source: "geojson",
-          paint: {
-            "circle-radius": 8,
-            "circle-color": "#00a9e2",
-          },
-          filter: ["in", "$type", "Point"],
-        });
-        this.map.addLayer({
-            id: 'measure-lines',
-            type: 'line',
-            source: 'geojson',
-            layout: {
-                'line-cap': 'round',
-                'line-join': 'round'
-            },
+        this.map.on("load", () => {
+            this.map.addSource("geojson", {
+            type: "geojson",
+            data: this.geojson,
+            });
+            this.map.addLayer({
+            id: "measure-points",
+            type: "circle",
+            source: "geojson",
             paint: {
-                'line-color': '#00a9e2',
-                'line-width': 2.5
+                "circle-radius": 8,
+                "circle-color": "#00a9e2",
             },
-            filter: ['in', '$type', 'LineString']
+            filter: ["in", "$type", "Point"],
+            });
+            this.map.addLayer({
+                id: 'measure-lines',
+                type: 'line',
+                source: 'geojson',
+                layout: {
+                    'line-cap': 'round',
+                    'line-join': 'round'
+                },
+                paint: {
+                    'line-color': '#00a9e2',
+                    'line-width': 2.5
+                },
+                filter: ['in', '$type', 'LineString']
+            });
         });
-      });
+        this.map.on('sourcedataloading', (e) => {
+            if(e.isSourceLoaded && e.source.data.features.length > 1) {
+                const features = e.source.data.features;
+                const linestring = features.map(
+                    (point) => point.geometry.coordinates
+                );
+
+                const pt1 = linestring[0];
+                const pt2 = linestring[1];
+
+                const arc = greatCircle(pt1, pt2);
+
+                this.geojson.features.push(arc);
+
+                let index = features.findIndex(
+                    (el) => el.geometry.type === "LineString"
+                );
+            } else if (e.isSourceLoaded && e.source.data.features.length < 2) {
+                /* let index = features.findIndex(
+                    (el) => el.geometry.type === "LineString"
+                );
+                features.splice(index, 1); */
+                console.log('Remove arc.');
+            }
+        });
     },
     addPoint(feature) {
       const pt = point([feature.latitude, feature.longitude], {
@@ -109,19 +125,6 @@ export default defineComponent({
       this.geojson.features.splice(index, 1);
       this.map.getSource("geojson").setData(this.geojson);
     },
-    getRoute() {
-        if(this.geojson.features.length > 1) {
-            this.linestring.geometry.coordinates = this.geojson.features.map(
-                (point) => point.geometry.coordinates
-            );
-            this.geojson.features.push(this.linestring);
-            this.map.getSource("geojson").setData(this.geojson);
-            
-            
-            console.log(this.linestring);
-            console.log("Route set.");
-        }
-    }
   },
   props: ["departure", "arrival"],
 });
