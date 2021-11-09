027d6111bc83ca8574e41f81dda4dfa1ec354c21
alexandru ana <anaalexandru1996@gmail.com>
2021-11-09 22:38:46 +0100

    Styled CounterInput.

--- components/CounterInput.vue
+++ components/CounterInput.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="counter-input">
-        <label class="block">
-            <input
-                type="number"
-                class="mt-1 p-2 block min-w-full border-0 bg-transparent"
-                autocomplete="off"
-                :placeholder="placeholder"
-                v-model.trim="pax"
-            >
-        </label>
+    <div class="counter-input h-10 w-32">
+            <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                <button data-action="decrement" class="bg-jet-blue-light text-white hover:text-white hover:bg-jet-blue h-full w-20 rounded-l cursor-pointer outline-none transition-all" @click="decrement">
+                    <span class="m-auto text-2xl font-thin">−</span>
+                </button>
+                <input type="number" :value="this.pax" v-bind="this.pax" class="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none" name="custom-input-number"></input>
+                <button data-action="increment" class="bg-jet-blue-light text-white hover:bg-jet-blue h-full w-20 rounded-r cursor-pointer transition-all" @click="increment">
+                    <span class="m-auto text-2xl font-thin">+</span>
+                </button>
+        </div>
     </div>
 </template>
 
@@ -18,7 +18,15 @@ import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
     data() {
         return {
-            pax: null
+            pax: 0
+        }
+    },
+    methods: {
+        increment: function() {
+            return this.pax++;
+        },
+        decrement: function() {
+            return this.pax--;
         }
     },
     props: [
@@ -28,4 +36,17 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+    input[type='number']::-webkit-inner-spin-button,
+    input[type='number']::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    .custom-number-input input:focus {
+        outline: none !important;
+    }
+
+    .custom-number-input button:focus {
+        outline: none !important;
+    }
 </style>
--- components/PairingSearch.vue
+++ components/PairingSearch.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid sm:grid-cols-1 md:grid-cols-4 md:gap-5 rounded-lg border-2 max-w-4xl py-8 px-5 mt-5 bg-gray-50"
+    class="grid grid-flow-col auto-cols-max gap-5 py-8 px-5 mt-5 bg-gray-50 rounded-lg"
   >
     <SearchInput
       icon="aircraft-depart"
