519853ca2f218a72a3d6a429014e3b0de87d971f
alexandruana <anaalexandru1996@gmail.com>
2022-08-16 13:38:51 +0200

    Added Hero Banner container.

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="hero__banner h-screen relative bg-center bg-cover bg-blend-darken overflow-hidden">
-		<div class="max-w-7xl mx-auto sm:py-12 lg:py-20 xl:py-28">
+	<div class="hero__banner h-screen flex flex-col justify-center relative bg-center bg-cover overflow-hidden">
+		<div class="container mx-auto sm:py-12 lg:py-20 xl:py-28">
 			<div class="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
 				<div class="mx-auto max-w-7xl px-4 sm:px-6 sm:mt-12 lg:px-8 md:mt-16 lg:mt-20 xl:mt-28">
 					<div class="sm:text-center lg:text-left">
