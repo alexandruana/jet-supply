ec76a1ea46c02c736b837e058530ee860ac988d6
alexandruana <anaalexandru1996@gmail.com>
2022-08-27 10:54:41 +0200

    Added custom drop shadow.

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -6,11 +6,11 @@
 			<div class="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
 				<div class="mx-auto max-w-7xl px-4 sm:px-6 sm:mt-12 lg:px-8 md:mt-16 lg:mt-20 xl:mt-28">
 					<div class="sm:text-center lg:text-left">
-						<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 drop-shadow-xl sm:text-5xl md:text-6xl">
+						<h1 class="text-4xl filter drop-shadow-custom tracking-tight font-extrabold text-gray-900 drop-shadow-xl sm:text-5xl md:text-6xl">
 							<span class="block xl:inline">Taking you places</span>
 							<span class="block text-jet-light xl:inline">wherever, whenever.</span>
 						</h1>
-						<p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+						<p class="mt-3 filter drop-shadow-custom text-base text-gray-300 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
 							Get access to over 3.000 aircraft worldwide and plan your trips to over 73.000 unique
 							airport pairs with our tailor made
 							travel solutions.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,32 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {
-        colors: {
-          'jet': {
-            light:'#00A9E2',
-            DEFAULT: '#3A3F55',
-            dark: '#2D3142'
-          }
-        },
-        backgroundImage: {
-          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        }
-      },
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				'jet': {
+					light: '#00A9E2',
+					DEFAULT: '#3A3F55',
+					dark: '#2D3142'
+				}
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+			},
+			dropShadow: {
+				'custom': [
+					'0 2px 7px rgba(0, 0, 0, 0.5)',
+					'0 2px 7px rgba(0, 0, 0, 0.2)',
+				],
+				'4xl': [
+					'0 35px 35px rgba(0, 0, 0, 0.25)',
+					'0 45px 65px rgba(0, 0, 0, 0.15)'
+				]
+			}
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
