b7bc85ca4514e26e8a00c1d75b7837e1af90c39f
alexandru ana <anaalexandru1996@gmail.com>
2022-09-29 22:31:52 +0200

    Removed form '@submit.prevent'

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -82,7 +82,7 @@
 				method="POST"
 				class="mx-auto"
 				action="/"
-				@submit.prevent="toggle">
+			>
 				<p class="text-lg mb-6">Send us a message</p>
 				<div class="flex flex-wrap -mx-3">
 					<input type="hidden" name="contact" value="contact">
