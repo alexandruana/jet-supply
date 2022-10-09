69524a43f12da52e7fdd54e43504e717a5cb6db4
alexandru ana <anaalexandru1996@gmail.com>
2022-10-09 16:32:17 +0200

    Added third input

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -117,6 +117,21 @@
 						>
 					</div>
 				</div>
+				<div class="flex flex-wrap -mx-3 mb-3 md:mb-6">
+					<div class="w-full px-3">
+						<label class="block uppercase tracking-wide text-gray-700 text-xs font-normal mb-2" for="email">
+							E-mail
+						</label>
+						<input
+							id="email"
+							class="appearance-none block w-full bg-transparent text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+							type="email"
+							name="email"
+							placeholder="jane@doe.com"
+							required
+						>
+					</div>
+				</div>
 			</form>
 		</div>
 	</div>
---
+++ dist/_nuxt/static/1665325928/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002FPrivacyPolicy","\u002F","\u002FTestPage","\u002Fprivacypolicy"]})
