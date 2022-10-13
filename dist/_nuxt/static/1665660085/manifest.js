53f086b584f0ced21209c975ba2fcda0742bf8f5
alexandru ana <anaalexandru1996@gmail.com>
2022-10-13 13:30:58 +0200

    Removed Home meta.

--- dist/_nuxt/static/1665660085/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FTestPage","\u002FPrivacyPolicy","\u002Fprivacypolicy"]})
--- pages/index.vue
+++ pages/index.vue
@@ -20,14 +20,6 @@ export default {
 	head () {
 		return {
 			title: 'Home',
-			meta: [
-				// hid is used as unique identifier. Do not use `vmid` for it as it will not work
-				{
-					hid: 'description',
-					name: 'description',
-					content: ''
-				}
-			]
 		}
 	},
 	components: { ContactForm }
