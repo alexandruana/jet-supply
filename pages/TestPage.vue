bd6a8a91261266a3bd978054a39664f3806c795a
alexandru ana <anaalexandru1996@gmail.com>
2022-10-16 13:41:43 +0200

    Added Prettier / ran npx prettier --write

--- pages/TestPage.vue
+++ pages/TestPage.vue
@@ -1,31 +1,69 @@
 <template>
-	<main class="max-w-7xl mx-auto">
-		<FAQComponent></FAQComponent>
+	<main class="max-w-7xl mx-auto"></main>
+		<label for="file-upload" class="custom-file-upload"> Open PDF </label>
+		<input id="file-upload" type="file" @change="openDocument" class="btn" />
+		<PSPDFKitContainer :pdfFile="pdfFile" @loaded="handleLoaded" />
 	</main>
 </template>
 
 <script>
-import FAQComponent from '~/components/FAQComponent.vue'
 export default {
-	components: { FAQComponent },
-	name: 'TestArea',
-	head () {
-		return {
-			title: 'TestArea',
-			meta: [
-				// hid is used as unique identifier. Do not use `vmid` for it as it will not work
-				{
-					hid: 'description',
-					name: 'description',
-					content: 'Test page'
-				}
-			]
-		}
+	name: "TestArea",
+	head() {
+	return {
+		title: "TestArea",
+		meta: [
+		// hid is used as unique identifier. Do not use `vmid` for it as it will not work
+		{
+			hid: "description",
+			name: "description",
+			content: "Test page",
+		},
+		],
+	};
 	},
-	data () {
+	data() {
 		return {
-			show: true
+			show: true,
+			pdfFile: this.pdfFile || "/document.pdf"
+		};
+	},
+	methods: {
+		handleLoaded(instance) {
+			console.log("PSPDFKit has loaded: ", instance);
+			// Do something.
+		},
+
+		openDocument(event) {
+			if (this.pdfFile && this.pdfFile.startsWith("blob:")) {
+				window.URL.revokeObjectURL(this.pdfFile);
+			}
+			this.pdfFile = window.URL.createObjectURL(event.target.files[0]);
 		}
 	}
-}
+};
 </script>
+
+<style scoped>
+body {
+  	margin: 0;
+}
+
+input[type="file"] {
+  	display: none;
+}
+
+.custom-file-upload {
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	display: inline-block;
+	padding: 6px 12px;
+	cursor: pointer;
+	background: #4a8fed;
+	padding: 10px;
+	color: #fff;
+	font: inherit;
+	font-size: 16px;
+	font-weight: bold;
+}
+</style>
