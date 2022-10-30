87d3eedbdb5e3d451448383f0ebb48e652885410
alexandru ana <anaalexandru1996@gmail.com>
2022-10-30 13:33:12 +0100

    Revert default favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
       },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
