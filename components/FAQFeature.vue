c6e510fab2e2af8b979caf62612e9c5dd39e6a6b
alexandru ana <anaalexandru1996@gmail.com>
2022-10-30 12:44:42 +0100

    Update FAQFeature mobile spacing.

--- components/FAQFeature.vue
+++ components/FAQFeature.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 		<div class="grid grid-cols-1 md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-			<div class="col-span-1">
+			<div class="col-span-1 mb-14 md:mb-0">
 				<h2
 					class="
 						text-base text-jet-light
