a3747eced0a5ae78aa9fb36811f3c0d636c0fced
alexandru ana <anaalexandru1996@gmail.com>
2022-10-30 12:43:40 +0100

    Update FAQ style.

--- components/FAQComponent.vue
+++ components/FAQComponent.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="accordion__item mb-3 py-4 px-4 border-b  border-slate-200">
+	<div class="accordion__item mb-3 py-4 border-b  border-slate-200">
 		<div
 			class="accordion__trigger text-lg font-medium flex justify-between cursor-pointer"
 			:class="{ accordion__trigger_active: visible }"
