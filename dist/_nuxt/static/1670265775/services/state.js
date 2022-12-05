bcfe0e896bd8ba7053a5f0c036565e0bc21ddc73
alexandru ana <anaalexandru1996@gmail.com>
2022-12-05 20:12:20 +0100

    Fixed Services page spacing.

--- dist/_nuxt/static/1670265775/services/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1670265775",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},_img:{"/_ipx/_/images/pattern.svg":"\u002F_nuxt\u002Fimage\u002F12c84f.svg"}}}(null));
