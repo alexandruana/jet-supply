81f4228be90dc0892d4d0ff8f9b41f2ec2fddce1
alexandru ana <anaalexandru1996@gmail.com>
2022-12-05 22:49:24 +0100

    Update Services copy.

--- dist/_nuxt/static/1670273006/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Ffaq","\u002Fservices","\u002FPrivacyPolicy","\u002F","\u002Fabout","\u002Fprivacypolicy"]})
---
+++ dist/_nuxt/static/1670276870/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Ffaq","\u002F","\u002Fservices","\u002Fabout","\u002FPrivacyPolicy","\u002Fprivacypolicy"]})
